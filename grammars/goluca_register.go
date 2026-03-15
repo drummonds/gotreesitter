cddf3692436be63e0d14103d41553c2ed8518f22
Humphrey Drummond <hum3@drummond.info>
2026-03-15 19:30:10 +0000

    updating goluca grammar

--- grammars/goluca_register.go
+++ grammars/goluca_register.go
@@ -12,13 +12,49 @@ func init() {
 
 const golucaHighlightQuery = `
 (date) @constant
+(time) @constant
+(timezone) @constant
+(tz_offset) @constant
+(knowledge_datetime "%" @operator)
+(period_anchor) @operator
+
 (flag) @keyword
+
 (payee) @string
+
 (account) @type
+
 (arrow) @operator
+
 (linked_prefix) @operator
+
 (description) @string
+
 (amount) @number
+
 (commodity) @constant
+
 (comment) @comment
+
+(metadata_key) @property
+(metadata_value) @string
+
+(param_name) @property
+(param_value) @string
+
+(option_key) @property
+(option_value) @string
+
+(alias_name) @variable
+
+(customer_name) @string
+
+"commodity" @keyword
+"open" @keyword
+"option" @keyword
+"alias" @keyword
+"customer" @keyword
+"data" @keyword
+"account" @keyword
+"max-aggregate-balance" @keyword
 `
